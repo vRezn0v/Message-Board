85589c4267cebf3c5fe94640e06e3deff784fa09
Abhinav Tomar <atomar.cse18@chitkarauniversity.edu.in>
2019-08-03 22:23:02 +0530

    Added Documentation

--- msgboard.py
+++ msgboard.py
@@ -1,6 +1,7 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import parse_qs
 
+#Messages are stored here
 memory = []
 
 form='''<!DOCTYPE html>
@@ -17,23 +18,27 @@ form='''<!DOCTYPE html>
 
 class MessageHandler(BaseHTTPRequestHandler):
     def do_POST(self):
+        #reads length of content from POST header
         length = int(self.headers.get('Content-Length',0))
+        #reads the data from POST request and stores it
         data = self.rfile.read(length).decode()
+        #parses data and extracts message
         message = parse_qs(data)["message"][0]
-        #self.send_response(200)
-        #self.send_header('Content-type', 'text/plain; charset=utf-8')
-        #self.end_headers()
-        #self.wfile.write(message.encode())
+        #formats message
         message = message.replace("<", "&lt;")
+        #adds message to memory
         memory.append(message)
+        #sends a redirect to MessageBoard home
         self.send_response(303)
         self.send_header("Location", '/')
         self.end_headers()
 
     def do_GET(self):
+        #Sends OK response
         self.send_response(200)
         self.send_header('Content-type', 'text/html; charset=utf-8')
         self.end_headers()
+        #prints messages from memory to screen
         mes=form.format("\n".join(memory))
         self.wfile.write(mes.encode())
 
