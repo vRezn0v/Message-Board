fcfde02c2f412855fe79bc2509e5ea5264dc795c
Abhinav Tomar <atomar.cse18@chitkarauniversity.edu.in>
2019-08-03 19:14:36 +0530

    GET Method Working

--- msgboard.py
+++ msgboard.py
@@ -1,6 +1,7 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import parse_qs
 
+html_form='<!DOCTYPE html><title>Message Board</title><form method="POST" action="http://localhost:8000/"><textarea name="message"></textarea><br><button type="submit">Post it!</button></form>'
 
 class MessageHandler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -13,6 +14,10 @@ class MessageHandler(BaseHTTPRequestHandler):
         self.wfile.write(message.encode())
 
     def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(html_form.encode())
 
 if __name__=='__main__':
     server_address=('',8000)
